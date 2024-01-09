549ba1ce93d34a1fd343dd6b9cefdb4b53d9880e
Mangui <antonio.mangado@gmail.com>
2024-01-10 00:03:12 +0100

    ADD/ formatting

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const session = require('express-session');
 const morgan = require('./middlewares/morgan');
+const path = require('path')
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+const usersRoutes = require("./routes/users.routes");
 
 app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
-
 app.use(express.json());
 
 
@@ -14,7 +14,6 @@ app.get("/", (req,res)=>{
     res.send("Hello World");
 })
 
-const usersRoutes = require("./routes/users.routes");
 
 
 app.use('/api/', usersRoutes);
