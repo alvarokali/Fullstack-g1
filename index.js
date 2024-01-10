d6816055c96e5adcee3ba5b0cb3314a110bd3769
Mangui <antonio.mangado@gmail.com>
2024-01-10 11:03:05 +0100

    ADD/ enable form data reception

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ const usersRoutes = require("./routes/users.routes");
 
 app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
 
 app.get("/", (req,res)=>{
     res.send("Hello World");
 })
 
-
-
 app.use('/api/', usersRoutes);
-
 
 app.listen(PORT, () => {
     console.log(`>Listening on port: http://localhost:${PORT}`);
